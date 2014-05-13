1b17e9bbee421da0998d952c3a12e169c2a5aa0c
Dino Prašo <desmond1303@gmail.com>
2014-05-13 11:06:00 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -36,5 +36,27 @@ namespace DMV_GUI
         {
 
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MotorVehicle mv = null;
+            if (radioButtonTruck.Checked)
+            {
+                mv = new Truck(textBoxVIN.Text, textBoxMake.Text, textBoxModel.Text, (int) NoOfSeets.Value, (int)NoOfWheels.Value,dateTimePicker1.Value,Convert.ToDouble(tbAddInfo.Text) );
+            }
+
+            if (radioButtonCar.Checked)
+            {
+                mv = new Car(textBoxVIN.Text, textBoxMake.Text, textBoxModel.Text, (int)NoOfSeets.Value, (int)NoOfWheels.Value, dateTimePicker1.Value,"Red", false, 4);
+            }
+
+            vehicles[count++] = mv;
+
+            richTextBox1.Clear();
+            foreach(MotorVehicle m in vehicles)
+            {
+                if (m != null) richTextBox1.AppendText(m.show() + '\n');
+
+            }
+        }
     }
 }
