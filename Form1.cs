560daa16b2fb64dae3a32e99a09cd33f13e8b25e
anrc1530 <anrc1530@aubih.edu.ba>
2014-05-13 11:13:16 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,10 @@ namespace DMV_GUI
 {
     public partial class Form1 : Form
     {
+
+        MotorVehicle[] vehicles = new MotorVehicle[20];
+        int count = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -36,27 +40,76 @@ namespace DMV_GUI
         {
 
         }
-        private void button1_Click(object sender, EventArgs e)
+
+        private void radioButtonTruck_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label1.Text = "maximum weight";
+            textBox1.Visible = true;
+        }
+
+        private void radioButtonBus_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label1.Text = "Company name";
+            textBox1.Visible = true;
+        }
+
+        private void radioButtonCar_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label1.Text = "Car Color";
+            textBox1.Visible = true;
+            label2.Visible = true;
+            label2.Text = "Number of airbags";
+            textBox2.Visible = true;
+            label3.Visible = true;
+            label3.Text = "Does the car have AC?";
+            radioButtonYes.Visible = true;
+            radioButtonNo.Visible = true;
+        }
+
+        private void radioButtonTaxi_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label1.Text = "Car Color";
+            textBox1.Visible = true;
+            label2.Visible = true;
+            label2.Text = "Number of airbags";
+            textBox2.Visible = true;
+            label3.Visible = true;
+            label3.Text = "Driver has licence?";
+            radioButtonYes.Visible = true;
+            radioButtonNo.Visible = true;
+
+        }
+        
+
+
+
+        private void radioButtonYes_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRegVeh_Click(object sender, EventArgs e)
         {
             MotorVehicle mv = null;
-            if (radioButtonTruck.Checked)
-            {
-                mv = new Truck(textBoxVIN.Text, textBoxMake.Text, textBoxModel.Text, (int) NoOfSeets.Value, (int)NoOfWheels.Value,dateTimePicker1.Value,Convert.ToDouble(tbAddInfo.Text) );
-            }
-
-            if (radioButtonCar.Checked)
-            {
-                mv = new Car(textBoxVIN.Text, textBoxMake.Text, textBoxModel.Text, (int)NoOfSeets.Value, (int)NoOfWheels.Value, dateTimePicker1.Value,"Red", false, 4);
+            if(radioButtonTruck.Checked)
+            {                                                                         //cast                    //cast
+                mv = new Truck(textBoxVIN.Text, textBoxMake.Text, textBoxModel.Text, (int)NoOfWheels.Value, (int)NoOfSeats.Value, dateTimePicker1.Value, Convert.ToDouble(textBox1.Text));
             }
 
             vehicles[count++] = mv;
-
+            
             richTextBox1.Clear();
+
             foreach(MotorVehicle m in vehicles)
             {
                 if (m != null) richTextBox1.AppendText(m.show() + '\n');
-
             }
         }
+
+
     }
 }
